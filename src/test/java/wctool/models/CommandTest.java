7a46e257249492ff789a7c9173aa28891c5ce7b4
chaitanyawaikar1993 <chaitanyawaikar1993@gmail.com>
2023-08-26 10:38:07 +0200

    - Add junit dependency - Add tests for parsing commands from input.

---
+++ src/test/java/wctool/models/CommandTest.java
@@ -0,0 +1,73 @@
+package wctool.models;
+
+import org.junit.jupiter.api.Test;
+import wctool.exception.InvalidNumberOfArgumentsException;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CommandTest {
+
+    @Test
+    public void testCommandCreationWithValidInput() {
+        //given
+        String inputLine = "CommandName param1 param2 param3";
+
+        //when
+        Command command = new Command(inputLine);
+
+        //then
+        List<String> params = command.getParams();
+        assertEquals("commandname", command.getCommandName());
+        assertEquals(3, params.size());
+        assertEquals("param1", params.get(0));
+        assertEquals("param2", params.get(1));
+        assertEquals("param3", params.get(2));
+    }
+
+    @Test
+    public void testCommandCreationWithNoParams() {
+        // given
+        String inputLine = "CommandName";
+
+        // when
+        Command command = new Command(inputLine);
+
+        // then
+        List<String> params = command.getParams();
+        assertEquals("commandname", command.getCommandName());
+        assertEquals(0, params.size());
+    }
+
+    @Test
+    public void testCommandCreationWithMultipleSpaces() {
+        // given
+        String inputLine = "  CommandName   param1   param2   ";
+
+        // when
+        Command command = new Command(inputLine);
+
+        // then
+        List<String> params = command.getParams();
+        assertEquals("commandname", command.getCommandName());
+        assertEquals(2, params.size());
+        assertEquals("param1", params.get(0));
+        assertEquals("param2", params.get(1));
+    }
+
+    @Test
+    public void testCommandCreationWithEmptyInput() {
+        assertThrows(InvalidNumberOfArgumentsException.class, () -> {
+            new Command("");
+        });
+    }
+
+    @Test
+    public void testCommandCreationWithOnlySpaces() {
+        assertThrows(InvalidNumberOfArgumentsException.class, () -> {
+            new Command("   ");
+        });
+    }
+}
